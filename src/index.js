23d3d64f462c67c0994e2a0fee461a9b9e9b466d
Paul Kim <paulwkim30@gmail.com>
2021-07-26 16:05:11 -0700

    try to get nba api response

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { testapi } from "./scripts/nba_api/shotchart_data"
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("year").innerHTML = "Select a year";
+
+  // testing
+  window.testapi = testapi;
+  // end testing
+})
+
+
+// createElement
