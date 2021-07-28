54d4d3250eed491a522d4669b8529d2f132eef29
Paul Kim <paulwkim30@gmail.com>
2021-07-28 11:01:34 -0700

    get shotts to display on d3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import { testapi } from "./scripts/nba_api/shots"
 import * as d3 from "d3";
-import fetch from "node-fetch";
-import { getShotLocations } from "./scripts/nba_api/shots"
+import Shots from "./scripts/nba_api/shots"
+
 
 document.addEventListener("DOMContentLoaded", () => {
   // document.getElementById("year").innerHTML = "Select a year";
@@ -10,40 +9,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const svg = d3.select("#shot-chart-container")
     .append("svg")                    
       .attr('width', 500)             
-      .attr('height', 500)
+      .attr('height', 900)
 
-  svg.append("rect")
-    .attr("width", "100%")
-    .attr("height", "100%")
-    .style("stroke", "red")
-    .style("stroke-width", 2)
-    .style("fill", "none")
+  // create new shot instance to plot shots
+  let shots = new Shots(svg, "2020-21", "201939")
+  shots.display()
 
-  const data = [
-    [10, 20], [20, 100], [200, 50],
-    [25, 80], [10, 200], [150, 75],
-    [10, 70], [30, 150], [100, 15],
-    [5,5], [10,5], [1,1]
-  ]
-
-  svg.append("g")
-    .selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-      .attr("cx", function (d) { return d[0]; } )
-      .attr("cy", function (d) { return d[1]; } )
-      .attr("r", 2)
-      .attr('transform', 'translate(250, 52.5)')
-      .style("fill", "#69b3a2")
+  
 
   
   // testing
 
   
-
-
-  getShotLocations("2020-21", "201939")
 
 
   // const fruits = ['apple', 'mango', 'banana', 'orange'];
--- src/scripts/nba_api/shots.js
+++ src/scripts/nba_api/shots.js
@@ -1,26 +1,43 @@
-// class Shots {
-//   constructor(svg, playerId, ) {
-//     this.svg = svg
-//   }
+class Shots {
+  constructor(svg, yearId, playerId) {
+    this.svg = svg;
+    this.yearId = yearId;
+    this.playerId = playerId;
+  }
 
+  display() {
+    fetch(`/getShotChart/${this.yearId}/${this.playerId}`)
+      .then( apiResponse => apiResponse.json() )
+      .then( data => {
+        let result = data.resultSets[0].rowSet;
+        let coordinates = result.map( ele => [ele[17], ele[18]])
+        console.log(coordinates);
 
-// }
+        this.svg.append("rect")
+          .attr("width", "100%")
+          .attr("height", "100%")
+          .style("stroke", "red")
+          .style("stroke-width", 2)
+          .style("fill", "none")
 
-
-
-let shotLocations;
-
-export const getShotLocations = (yearId, playerId) => {
-  fetch(`/getShotChart/${yearId}/${playerId}`)
-    .then( apiResponse => apiResponse.json() )
-    .then( data => {
-      shotLocations = data.resultSets[0].rowSet;
-      console.log(shotLocations)
-    })
+        this.svg.append("g")
+          .selectAll("circle")
+          .data(coordinates)
+          .enter()
+          .append("circle")
+            .attr("cx", function (d) { return d[0]; } )
+            .attr("cy", function (d) { return d[1]; } )
+            .attr("r", 2)
+            .attr('transform', 'translate(250, 52.5)')
+            .style("fill", "#69b3a2")
+    })    
+  }
 }
 
+export default Shots;
 
 
+
+// test data:
 // 201939
 // 2020-21
-
