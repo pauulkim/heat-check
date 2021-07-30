d1f79c6b8368ab6d9679fc9695bd0803e9ff5de3
Paul Kim <paulwkim30@gmail.com>
2021-07-30 07:05:24 -0700

    clean up code

--- src/scripts/other_info/dropdown.js
+++ src/scripts/other_info/dropdown.js
@@ -2,8 +2,9 @@ import * as d3 from "d3";
 
 // DROPDOWN
 const dropdown = (type, selections) => {
+  d3.select(`#${type}-select-container`).append("p").text(`Select a ${type.slice(0, -1)}`);
   let selectEle = d3.select(`#${type}-select-container`).append("select");
-
+  
   if (type === "years") {
     selections.forEach( year => {
       let option = selectEle.append("option");
